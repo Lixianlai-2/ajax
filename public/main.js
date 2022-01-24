c4f7819cc99e1e1063bf6831d923f2b05cd527f5
MarkLee <1643565650@qq.com>
2022-01-24 12:06:53 +0800

    html/css/html/xml/json的ajax部分建设完毕

--- public/main.js
+++ public/main.js
@@ -26,6 +26,7 @@ getJS.onclick = () => {
   //注意这里打开的路径是server.js里path的路径
   request.open("get", "/2.js");
   request.onload = () => {
+    console.log(request.readyState);
     console.log("请求js成功了");
     console.log(request.response);
     const script = document.createElement("script");
@@ -42,22 +43,47 @@ getJS.onclick = () => {
 
 // 点击得到css样式
 getCSS.onclick = (e) => {
-  console.log(e);
-  console.log("这里成功吗");
+  console.log("getCSS成功了吗");
   const request = new XMLHttpRequest();
   // 这里是网页输入时的路径吗？跟server.js里的path路径一致
   request.open("get", "/style.css");
 
-  request.onload = () => {
-    console.log("request onload");
-    const style = document.createElement("style");
-    style.innerHTML = request.response;
-    document.head.appendChild(style);
+  request.onreadystatechange = () => {
+    // 注意，这里是用的箭头函数，所有不能直接用this.readyState，不然是返回undefined
+    console.log(request.readyState);
+    // readyState为4时下载完成，然后通过请求的status判断是否请求成功
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      }
+    }
   };
+  request.send();
+};
 
-  request.onerror = () => {
-    console.log("request onerror");
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      console.log(request.responseXML);
+    }
   };
+  request.send();
+};
 
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const Object = JSON.parse(request.response);
+      console.log(Object);
+    }
+  };
   request.send();
 };
